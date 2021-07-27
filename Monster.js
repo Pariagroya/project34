28ee96326444a2645006d23e689b21f77f997413
Pariagroya <82873373+Pariagroya@users.noreply.github.com>
2021-07-27 10:47:16 +0530

    Update Monster.js

--- Monster.js
+++ Monster.js
@@ -6,7 +6,7 @@ var options={
     frictionAir:0.1,
    
 }
-this.image=loadImage("Monster.png")
+this.image=loadImage("monster.png")
 this.body=Bodies.rectangle(x,y,width,height,options)
 
 this.width=width
